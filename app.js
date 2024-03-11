d87f004edf0b7f5e1c688d1b121c17a08c512b8c
ibr <ibr.seckin@gmail.com>
2024-03-11 22:21:02 +0300

    stop fix

--- app.js
+++ app.js
@@ -358,18 +358,20 @@ const setColorTheme = (choosenColor) => {
   document.querySelector("header").style.backgroundColor = choosenColor;
   document.querySelector("#joke .container").style.backgroundColor = choosenColor;
   document.querySelector("#joke .container").style.color = choosenColor;
-  document.querySelector("main section .container").style.borderColor = choosenColor;
+  document.querySelectorAll("main section .container").forEach(item => item.style.borderColor = choosenColor);
   document
   .querySelectorAll("section#count-time .container .time-buttons button")
   .forEach((item) => (item.style.backgroundColor = choosenColor));
 
+  document.querySelector("section#count-time .container").style.borderColor = choosenColor;
+    
   document.querySelector("section#tasks .container").style.borderColor = choosenColor;  
-  document.querySelector("section#task-container .container").style.borderColor = choosenColor;  
+    
 
-document.querySelector("#task-input").style.backgroundColor = choosenColor; 
-document.querySelector("#task-input").style.opacity = ".6"; 
-document.querySelector("#task-add-btn").style.backgroundColor = choosenColor; 
-document.querySelector(".active-task p").style.color = choosenColor; 
+  document.querySelector("#task-input").style.backgroundColor = choosenColor; 
+  document.querySelector("#task-input").style.opacity = ".6"; 
+  document.querySelector("#task-add-btn").style.backgroundColor = choosenColor; 
+  document.querySelector(".active-task p").style.color = choosenColor; 
 
 
   document
